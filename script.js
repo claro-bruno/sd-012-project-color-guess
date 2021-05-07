88f97e67c4608cf089cdf135dd59e231977a82ba
Gabriel Takaki <gabitj@icloud.com>
2021-05-07 17:56:50 -0300

    melhorias e lint

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const ballsEl = document.getElementById('main-ball');
 const colorRgb = document.getElementById('rgb-color');
 
 function createBalls() {
+  let res = [];
   for (let index = 0; index < 6; index += 1) {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16); // https://css-tricks.com/snippets/javascript/random-hex-color/
     const eachBall = document.createElement('div');
     eachBall.className = 'ball';
     eachBall.style.backgroundColor = '#' + randomColor;
+    res.push(eachBall.style.backgroundColor);
     ballsEl.appendChild(eachBall);
-    colorRgb.textContent = eachBall.style.backgroundColor;
+    colorRgb.textContent = res[Math.floor(Math.random() * res.length)];
   }
 }
 
@@ -18,14 +20,14 @@ function getColorValue() {
   const colorVal = document.querySelectorAll('.ball');
   const asnwer = document.getElementById('answer');
   colorVal.forEach((color) => {
-    color.addEventListener('click', (e) => {
+    color.addEventListener('click', () => {
       const att = color.style.backgroundColor;
       console.log(att);
       if (colorRgb.textContent === att) {
         asnwer.textContent = 'Acertou!';
       } else {
         asnwer.textContent = 'Errou! Tente novamente!';
-      }
+      };
     })
   })
 }
@@ -35,8 +37,8 @@ getColorValue();
 function resetGame() {
   const resetBtn = document.getElementById('reset-game');
   resetBtn.addEventListener('click', () => {
-    location.reload();
-  })
+    window.location.reload();
+  });
 }
 
 resetGame();
