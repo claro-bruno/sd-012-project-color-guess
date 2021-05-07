6aa90c09db47b80d88b686372a64fc8a2fd9714c
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-07 16:02:54 -0300

    adding generating answer color

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const bolasCores = document.querySelectorAll('.ball');
+const corDaResposta = document.querySelector('#rgb-color');
+
 function gerarCoresAleatorias() {
   const red = Math.ceil(Math.random() * 255);
   const green = Math.ceil(Math.random() * 255);
@@ -7,8 +9,18 @@ function gerarCoresAleatorias() {
   return `rgb(${red} , ${green} , ${blue})`
 }
 
+function escolherResposta() {
+  const teste = Math.ceil(Math.random() * 5);
+  let corSemRGB = `${bolasCores[teste].style.backgroundColor}`;
+  corSemRGB = corSemRGB.replace('rgb','');
+  corDaResposta.innerText= `${corSemRGB}`;
+}
+
+
+
 window.onload = () => {
   for (let index = 0; index < bolasCores.length; index += 1) {
     bolasCores[index].style.backgroundColor = gerarCoresAleatorias();
   }
+  escolherResposta();
 }
