a80d6e811ebc945bdd867b3604567ef752744308
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-08 15:56:53 -0300

    Requisitos 3 e 4 / Adiciona footer

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ function randomColors() {
   }
 }
 
-const randomColor = randomColors();
+function createColorOptions(numberOfColors) {
+  const getColorOptions = document.getElementById('color-options');
+  const colors = [];
+  for (let index = 0; index < numberOfColors; index += 1) {
+    const color = randomColors().color;
+    const createColorOption = document.createElement('div');
+    createColorOption.className = 'ball';
+    createColorOption.style.backgroundColor = color;
+    getColorOptions.appendChild(createColorOption);
+    colors.push(color);
+  }
+  return colors
+}
+
+
+const numberOfColors = 6;
+
+const colors = createColorOptions(numberOfColors);
+const randomIndex = parseInt(Math.random() * numberOfColors);
+const randomColor = colors[randomIndex];
 const getCorrectColorP = document.getElementById('rgb-color');
-getCorrectColorP.innerHTML = randomColor.colorNumbers; 
+getCorrectColorP.innerHTML = randomColor.replace('rgb',''); 
