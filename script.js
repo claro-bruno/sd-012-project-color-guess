473fbfea9b0453c9df16d5638f70885007f693a2
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-08 14:38:34 -0300

    Feito requisito 1

---
+++ script.js
@@ -0,0 +1,7 @@
+const content = document.createElement('div');
+document.body.appendChild(content);
+
+const title = document.createElement('h1');
+title.id = 'title';
+title.innerHTML = 'Adivinhe a cor!';
+content.appendChild(title);
