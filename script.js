e9d12201cdb16adad23c697ad8d51e954bf6830a
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-08 15:27:46 -0300

    Feito requisito 2

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const title = document.createElement('h1');
 title.id = 'title';
 title.innerHTML = 'Adivinhe a cor!';
 content.appendChild(title);
+
+const textColor = document.createElement('p');
+textColor.id = 'rgb-color';
+content.appendChild(textColor);
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function generateRGB() {
+  const red = randomNumber(0, 255);
+  const green = randomNumber(0, 255);
+  const blue = randomNumber(0, 255);
+
+  return `(${red}, ${green}, ${blue})`;
+}
+
+textColor.innerHTML = generateRGB();
