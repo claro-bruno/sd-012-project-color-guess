cc617ab3a3c98b5e56913ff062c373a95e3657a2
GMODias <gmd.delta@gmail.com>
2021-05-10 06:35:15 -0300

    [PT-BR]Resolvido requisitos 5 e 6.  [EN] Solved 5st and 6th requirements.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const colorTextplace = document.getElementById('rgb-color');
 const ballColorsplace = document.getElementById('colorsPlace');
+const mensagePlace = document.getElementById('answer');
+const resetBtn = document.getElementById('reset-game');
 
 function createRandomcolor() {
   const colorNum = [];
   for (let index = 0; index < 3; index += 1) {
     colorNum.push(Math.round(Math.random() * 255));
   }
-  return `(${colorNum[0]},${colorNum[1]},${colorNum[2]})`;
+  return `(${colorNum[0]}, ${colorNum[1]}, ${colorNum[2]})`;
 }
 
 function randomN(max) {
@@ -17,6 +19,7 @@ function startGame() {
   const qtd = 6;
   const colorToguess = createRandomcolor();
   const rightColorposition = randomN(6);
+  ballColorsplace.innerHTML = '';
   colorTextplace.innerText = colorToguess;
   for (let index = 0; index < qtd; index += 1) {
     const colorelmnt = document.createElement('div');
@@ -26,6 +29,24 @@ function startGame() {
     } else { colorelmnt.style.backgroundColor = `rgb${createRandomcolor()}`; }
     ballColorsplace.appendChild(colorelmnt);
   }
+  mensagePlace.innerText = 'Escolha uma cor';
 }
 
 window.onload = startGame;
+
+//
+function selectAnswer(event) {
+  if (event.target.tagName !== 'SECTION') {
+    console.log(event.target.style.backgroundColor, `rgb${colorTextplace.innerText}`);
+    if (event.target.style.backgroundColor === `rgb${colorTextplace.innerText}`) {
+      mensagePlace.innerText = 'Acertou!';
+    } else {
+      mensagePlace.innerText = 'Errou! Tente novamente!';
+    }
+  }
+}
+
+ballColorsplace.addEventListener('click', selectAnswer);
+
+// Resetar
+resetBtn.addEventListener('click', startGame);
