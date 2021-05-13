729c5c64e730d99b9d7e4f381fea19234c941705
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-13 20:15:37 -0300

    adiciona borda preta nas bolas

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let corASerAdivinhada = '(' + r + ', ' + g + ', ' + b + ')';
 let rgbColor = document.querySelector('#rgb-color');
 let circleContainer = document.querySelector('#circle-container');
 let numeroResposta = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
-// let options = document.querySelectorAll('.ball');
 
 rgbColor.innerText = corASerAdivinhada;
 
@@ -20,13 +19,12 @@ for (let index = 1; index <= 6; index += 1) {
   option.style.height = '50px';
   option.style.width = '50px';
   option.style.borderRadius = '50%';
+  option.style.border = '1px solid black';
   option.style.margin = '10px'
   if (index === numeroResposta) {
     option.style.backgroundColor = 'rgb' + corASerAdivinhada;
-    // options[index-1].style.backgroundColor = 'rgb' + corASerAdivinhada;
   } else {
     option.style.backgroundColor = corOption;
-    // options[index-1].style.backgroundColor = corOption;
   }
   circleContainer.appendChild(option);
 }
