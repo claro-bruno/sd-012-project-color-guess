666c69086874b631d261364ab41e24524851bd0a
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-13 19:49:37 -0300

    adiciona opçoes de cores

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let r = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
 let g = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
 let b = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
-
-let rgbColor = document.querySelector('#rgb-color');
-
 let corASerAdivinhada = '(' + r + ', ' + g + ', ' + b + ')';
+let rgbColor = document.querySelector('#rgb-color');
+let circleContainer = document.querySelector('#circle-container');
+let numeroResposta = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
 
 rgbColor.innerText = corASerAdivinhada;
+
+for (let index = 1; index <= 6; index += 1) {
+  let option = document.createElement('div');
+  let red = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
+  let green = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
+  let blue = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
+  let corOption = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+  option.style.display = 'inline-block';
+  option.className = 'ball';
+  option.style.height = '50px';
+  option.style.width = '50px';
+  option.style.borderRadius = '50%';
+  if (index === numeroResposta) {
+    option.style.backgroundColor = 'rgb' + corASerAdivinhada;
+  } else {
+    option.style.backgroundColor = corOption;
+  }
+  circleContainer.appendChild(option);
+}
