a5eaf635b17c64c9501ca73c4b9e235ee19ddc2e
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-13 20:08:36 -0300

    adiciona margem nas bolas

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let corASerAdivinhada = '(' + r + ', ' + g + ', ' + b + ')';
 let rgbColor = document.querySelector('#rgb-color');
 let circleContainer = document.querySelector('#circle-container');
 let numeroResposta = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
+// let options = document.querySelectorAll('.ball');
 
 rgbColor.innerText = corASerAdivinhada;
 
@@ -19,10 +20,13 @@ for (let index = 1; index <= 6; index += 1) {
   option.style.height = '50px';
   option.style.width = '50px';
   option.style.borderRadius = '50%';
+  option.style.margin = '10px'
   if (index === numeroResposta) {
     option.style.backgroundColor = 'rgb' + corASerAdivinhada;
+    // options[index-1].style.backgroundColor = 'rgb' + corASerAdivinhada;
   } else {
     option.style.backgroundColor = corOption;
+    // options[index-1].style.backgroundColor = corOption;
   }
   circleContainer.appendChild(option);
 }
