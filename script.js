ba628c37436def65ca83ebd099226c896b4e850a
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-13 20:27:14 -0300

    adiciona texto de resposta

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let r = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
 let g = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
 let b = (Math.floor(Math.random() * (255 - 0 + 1)) + 0).toString();
 let corASerAdivinhada = '(' + r + ', ' + g + ', ' + b + ')';
+let corCorreta = 'rgb' + corASerAdivinhada;
 let rgbColor = document.querySelector('#rgb-color');
 let circleContainer = document.querySelector('#circle-container');
+let answer = document.querySelector('#answer');
 let numeroResposta = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
 
 rgbColor.innerText = corASerAdivinhada;
@@ -22,9 +24,19 @@ for (let index = 1; index <= 6; index += 1) {
   option.style.border = '1px solid black';
   option.style.margin = '10px'
   if (index === numeroResposta) {
-    option.style.backgroundColor = 'rgb' + corASerAdivinhada;
+    option.style.backgroundColor = corCorreta;
   } else {
     option.style.backgroundColor = corOption;
   }
   circleContainer.appendChild(option);
 }
+
+function resposta(event) {
+  if (event.target.style.backgroundColor === corCorreta) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!'
+  }
+}
+
+circleContainer.addEventListener('click', resposta);
