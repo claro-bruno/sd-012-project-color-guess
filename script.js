6b095beb0e024d0b47b4186bccca4b119ed0d330
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-06-05 19:20:26 -0300

    exercicio 3 adiciona as cores para serem adivinhadas

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const rgbColor = () => {
   const color2 = Math.round(Math.random() * 255);
   const color3 = Math.round(Math.random() * 255);
   const spanRgb = document.querySelector('#rgb-color');
-  spanRgb.innerHTML = `rgb ( ${color}, ${color2}, ${color3})`;
+  spanRgb.innerHTML = `rgb( ${color}, ${color2}, ${color3})`;
 };
 
 rgbColor();
+
+const colors = () => {
+  const divColor = document.querySelectorAll('.ball');;
+  const randomColor = document.querySelector('#rgb-color').innerHTML;
+  divColor.forEach((cor) => {
+  const color = Math.round(Math.random() * 255);
+  const color2 = Math.round(Math.random() * 255);
+  const color3 = Math.round(Math.random() * 255);
+   console.log(cor.style.backgroundColor = `rgb( ${color}, ${color2}, ${color3})`);
+  })
+}
+
+colors();
